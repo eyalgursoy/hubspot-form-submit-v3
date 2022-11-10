febac9ad18d8e9caad762b9dc91430bb4b1b743b
Eyal Gursoy <eyal.gursoy@mayple.com>
2022-11-10 09:56:31 +0200

    remove build folder from codebase

--- build/types/consts.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const HUBSPOT_UTK_COOKIE = "hubspotutk";
-export declare const HUBSPOT_FORM_SUBMIT_URL = "https://api.hsforms.com/submissions/v3/integration/submit/";
--- build/types/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { FinalHubspotData, FormSubmitResponse, HubspotContext, HubspotData, HubspotField } from './types';
-declare const hubspotFormSubmit: (portalId: string, formGuid: string, fields: HubspotField[], context?: HubspotContext) => Promise<FormSubmitResponse>;
-export type { HubspotField, HubspotContext, HubspotData, FinalHubspotData };
-export default hubspotFormSubmit;
